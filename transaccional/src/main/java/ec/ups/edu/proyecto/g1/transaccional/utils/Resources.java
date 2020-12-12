edd82bec6c42dae6c5fbf44f7d6cf9ca5e60d57b
FernandoDeleg <56567621+FernandoDeleg@users.noreply.github.com>
2020-12-12 16:47:49 -0500

    Add files via upload

---
+++ transaccional/src/main/java/ec/ups/edu/proyecto/g1/transaccional/utils/Resources.java
@@ -0,0 +1,26 @@
+package ec.ups.edu.proyecto.g1.transaccional.utils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class Resources {
+
+	String dsName = "java:jboss/datasources/transaccionalDS";
+
+	@Produces
+	private Connection createConnection() throws SQLException, NamingException {
+		InitialContext ic = new InitialContext();
+		DataSource ds = (DataSource) ic.lookup(dsName);
+		return ds.getConnection();
+	}
+
+	private void closeConnection(@Disposes Connection conn) throws SQLException {
+		conn.close();
+	}
+}
